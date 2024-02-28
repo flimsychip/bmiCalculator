9d782f84741cc5bbb7be37baacfefc0f00fc58ed
allie <54782727+flimsychip@users.noreply.github.com>
2024-02-28 00:10:23 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,17 +3,19 @@ import java.util.Calendar;
 import java.text.SimpleDateFormat;
 
 public class Main {
-    static String name; static int ft; static int inches; static double weight; static double lowWeight; static double highWeight;
+    static String name; static int ft; static int inches; static double weight; static double lowWeight; static double highWeight; static double bmiCurrent; static String statusCurrent;
     static Scanner input = new Scanner(System.in);
     static final String highlight = "\u001B[43m"; static final String reset = "\u001B[0m";
 
     public static void main(String[] args) {
         welcome();
         getInputs();
-        double BMI = calculateBMI(ft, inches, weight);
-        displayResults(BMI);
+        bmiCurrent = calculateBMI(ft, inches, weight);
+        statusCurrent = calculateStatus(bmiCurrent);
+        displayResults(bmiCurrent, statusCurrent);
         getRange();
         displayTable(lowWeight, highWeight);
+        sayBye();
     }
 
     static void welcome() {
@@ -46,11 +48,10 @@ public class Main {
         }
     }
 
-    static void displayResults(double bmi) {
+    static void displayResults(double bmi, String status) {
         String date = new SimpleDateFormat("MMMM dd, yyyy").format(Calendar.getInstance().getTime());
         String time = new SimpleDateFormat("HH:mm:ss a").format(Calendar.getInstance().getTime());
-        String status = calculateStatus(bmi);
-        System.out.printf("\n-- SUMMARY REPORT for %s\n-- Date and Time: %21s at %s\n-- BMI: %23f (or %.1f if rounded)\n-- Weight Status: %18s\n\n", name, date, time, bmi, bmi, status);
+        System.out.printf("\n-- SUMMARY REPORT for %s\n-- Date and Time: %21s at %s\n-- BMI: %23f (or %.1f if rounded)\n-- Weight Status:     %s\n\n", name, date, time, bmi, bmi, status);
     }
 
     static void getRange() {
@@ -65,11 +66,24 @@ public class Main {
         double bmiLow = calculateBMI(ft, inches, low);
         String statusLow = calculateStatus(bmiLow);
         System.out.printf("|  %-10.2f|  %-10.2f|  %-18s" + highlight + "(LOW)" + reset + "  |\n", low, bmiLow, statusLow);
-        for(double i = low+5.5; i < high; i+= 5.5) {
+        if(low <= weight && weight <= low+5.5) {
+            System.out.printf("|  %-10.2f|  %-10.2f|  %-17s(this)  |\n", weight, bmiCurrent, statusCurrent);
+        }
+        for(double i = low+5.5; i < high; i += 5.5) {
             double bmi = calculateBMI(ft, inches, i);
             String status = calculateStatus(bmi);
             System.out.printf("|  %-10.2f|  %-10.2f|  %-25s|\n", i, bmi, status);
+            if(i <= weight && weight < i+5.5) {
+                System.out.printf("|  %-10.2f|  %-10.2f|  %-17s(this)  |\n", weight, bmiCurrent, statusCurrent);
+            }
         }
-        //high line
+        double bmiHigh = calculateBMI(ft, inches, high);
+        String statusHigh = calculateStatus(bmiHigh);
+        System.out.printf("|  %-10.2f|  %-10.2f|  %-17s" + highlight + "(HIGH)" + reset + "  |\n" + "-".repeat(55), high, bmiHigh, statusHigh);
+    }
+
+    static void sayBye() {
+        String goodbye = new String[] {"Goodbye", "Cheers", "Poopaye", "Adios", "Ciao", "Sayonara", "Au revoir"}[(int)(Math.random()*7)];
+        System.out.printf("\n\nThe SFSU Mashouf Wellness Center is at 755 Font Blvd.\n\n" + "-".repeat(90) + "\n-- Thank you for using my program, %s!\n-- %s!!!\n" + "-".repeat(90), name, goodbye);
     }
 }
